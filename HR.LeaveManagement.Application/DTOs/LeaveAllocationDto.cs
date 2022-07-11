155a97705cd14731d2e9959b89106726c9a99df6
Filip Dimitrijevic <dimitrijevicfilip995n@gmail.com>
2022-07-11 14:47:22 +0200

    Add project files.

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveAllocationDto : BaseDto
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
