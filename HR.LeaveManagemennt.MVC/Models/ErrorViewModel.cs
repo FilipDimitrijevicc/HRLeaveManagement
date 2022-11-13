382302cda3e315aba1c331d683c9fe17e79694ab
Filip Dimitrijevic <dimitrijevicfilip995n@gmail.com>
2022-11-13 14:15:06 +0100

    Add mvc app and much more

---
+++ HR.LeaveManagemennt.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HR.LeaveManagemennt.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
